1f77d33e9a2d73e0d93f1bee2d137c8bf3ca2339
Nils Bokermann <nils.bokermann@bermuda.de>
2025-04-14 18:26:28 +0200

    Minor corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./layouts/**/*.html"],
+    content: ["./layouts/**/*.html", "./assets/css/carousel.css"],
     darkMode: 'class',
     theme: {
         extend: {}
